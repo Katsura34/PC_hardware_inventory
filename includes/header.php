a6a4c69b69e07391c72d6e146477588b6a3d61ea
Katsura34 <157209871+Katsura34@users.noreply.github.com>
2025-11-29 23:13:34 +0800

    Revert "Add graph statistics to dashboard (#30)"
    This reverts commit 3ac2acddd17c81740a5e18bee540a256a6e339cf.

--- includes/header.php
+++ includes/header.php
@@ -30,9 +30,6 @@ if (!isset($pageTitle)) {
     <!-- HCI Theme Enhancements -->
     <link rel="stylesheet" href="<?php echo BASE_PATH; ?>assets/css/theme-hci.css">
     
-    <!-- Chart.js for Dashboard Graphs -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js" integrity="sha384-0S8rEECqrZDECM6j9yUtYSgU8dqj8FdChJPh/N6+4nxwJQU7X1MkJyVheByjZ3cu" crossorigin="anonymous"></script>
-    
     <!-- Base path for JavaScript -->
     <script>window.BASE_PATH = '<?php echo BASE_PATH; ?>';</script>
 </head>
